a906f5ad5c61cfe90552fc5391baae2f3170db55
ushergui <guilherme-02-08@hotmail.com>
2021-07-08 01:59:22 -0300

    Atualizando páginas de exclusão e edição

--- terrenos/urls.py
+++ terrenos/urls.py
@@ -6,5 +6,8 @@ from .views import *
 urlpatterns = [
     url(r'^cadastrar_estado', cadastrar_estado, name='cadastrar_estado'),
     url(r'^listar_estado', listar_estado, name='listar_estado'),
+    url(r'^listar_estado', listar_estado, name='listar_estado'),
+    url(r'^excluir_estado/(?P<pk>[0-9]+)', excluir_estado, name='excluir_estado'),
+    url(r'^editar_estado/(?P<pk>[0-9]+)', editar_estado, name='editar_estado'),
 
 ]
--- terrenos/views.py
+++ terrenos/views.py
@@ -2,13 +2,13 @@ from django.forms import ModelForm
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
-class estado_form(ModelForm):
+class formulario_estado(ModelForm):
     class Meta:
         model = Estado
         fields = ['nome_estado', 'abreviatura_estado']
 
-def cadastrar_estado(request, template_name='estado/estado_form.html'):
-    form = estado_form(request.POST or None)
+def cadastrar_estado(request, template_name='estado/formulario_estado.html'):
+    form = formulario_estado(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('listar_estado')
@@ -18,3 +18,21 @@ def listar_estado(request, template_name='estado/listar_estado.html'):
     estado = Estado.objects.all()
     estados = {'lista': estado}
     return render(request, template_name, estados)
+
+def excluir_estado(request, pk):
+    estado = Estado.objects.get(pk=pk)
+    if request.method == "POST":
+        estado.delete()
+        return redirect('listar_estado')
+    return render(request, 'estado/excluir_estado.html', {'estado': estado})
+
+def editar_estado(request, pk, template_name='estado/formulario_estado.html'):
+    estado = get_object_or_404(Estado, pk=pk)
+    if request.method == "POST":
+        form = formulario_estado(request.POST, instance=estado)
+        if form.is_valid():
+            estado = form.save()
+            return redirect('listar_estado')
+    else:
+        form = formulario_estado(instance=estado)
+    return render(request, template_name, {'form': form})
